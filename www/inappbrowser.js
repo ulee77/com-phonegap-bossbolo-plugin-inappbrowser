b5ce8e76bab8c599132836b501bcf81855cf284f
ULee <uwenlee77@gmail.com>
2015-11-25 14:38:11 +0800

    修复无法显示标题的BUG
    修复无法显示标题的BUG

--- www/inappbrowser.js
+++ www/inappbrowser.js
@@ -77,7 +77,7 @@ InAppBrowser.prototype = {
     }
 };
 
-module.exports = function(strUrl, strWindowName, strWindowFeatures, callbacks) {
+module.exports = function(strUrl, strWindowName, strWindowFeatures, header, callbacks) {
     // Don't catch calls that write to existing frames (e.g. named iframes).
     if (window.frames && window.frames[strWindowName]) {
         var origOpenFunc = modulemapper.getOriginalSymbol(window, 'open');
@@ -98,7 +98,7 @@ module.exports = function(strUrl, strWindowName, strWindowFeatures, callbacks) {
 
     strWindowFeatures = strWindowFeatures || "";
 
-    exec(cb, cb, "BoloBrowser", "open", [strUrl, strWindowName, strWindowFeatures]);
+    exec(cb, cb, "BoloBrowser", "open", [strUrl, strWindowName, strWindowFeatures, header]);
     return iab;
 };
 
